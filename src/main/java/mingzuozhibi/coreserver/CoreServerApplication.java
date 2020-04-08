77b93672ea9291b4c7663deffdfa882b89277762
fuhaiwei <fuhaiwei@126.com>
2020-04-08 16:23:03 +0800

    feat(project): Add from start.spring.io

---
+++ src/main/java/mingzuozhibi/coreserver/CoreServerApplication.java
@@ -0,0 +1,13 @@
+package mingzuozhibi.coreserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreServerApplication.class, args);
+	}
+
+}
