3bbe17756b6f8bd13aa67105ada79dc3d914d3ba
fuhaiwei <fuhaiwei@126.com>
2020-04-08 16:33:43 +0800

    style: format source code

--- src/main/java/mingzuozhibi/coreserver/CoreServerApplication.java
+++ src/main/java/mingzuozhibi/coreserver/CoreServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CoreServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CoreServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoreServerApplication.class, args);
+    }
 
 }
