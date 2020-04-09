1b5492be67844707742c6dc99d481629a704f370
fuhaiwei <fuhaiwei@126.com>
2020-04-10 06:14:55 +0800

    feat(auth): Remove not working @Component

--- src/main/java/mingzuozhibi/coreserver/auth/config/SecurityConfig.java
+++ src/main/java/mingzuozhibi/coreserver/auth/config/SecurityConfig.java
@@ -6,9 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.stereotype.Component;
 
-@Component
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
