b4fddb7a7d4594eafdaee776366960060995f390
fuhaiwei <fuhaiwei@126.com>
2020-04-12 10:20:14 +0800

    feat(token): Add @scheduled check token

--- src/main/java/mingzuozhibi/coreserver/modules/auth/token/TokenRepository.java
+++ src/main/java/mingzuozhibi/coreserver/modules/auth/token/TokenRepository.java
@@ -2,10 +2,14 @@ package mingzuozhibi.coreserver.modules.auth.token;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 public interface TokenRepository extends JpaRepository<Token, Long> {
 
     Optional<Token> findByUuid(String uuid);
+
+    List<Token> findByExpireOnBefore(Instant instant);
 
 }
