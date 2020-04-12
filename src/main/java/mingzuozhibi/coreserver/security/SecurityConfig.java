8cac16a187ec4168515411c7aeafee057cc6c2b6
fuhaiwei <fuhaiwei@126.com>
2020-04-12 10:23:31 +0800

    feat(slf4j): Remove debug unauth user

--- src/main/java/mingzuozhibi/coreserver/security/SecurityConfig.java
+++ src/main/java/mingzuozhibi/coreserver/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package mingzuozhibi.coreserver.security;
 
+import mingzuozhibi.coreserver.modules.auth.token.TokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,6 +10,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private TokenRepository tokenRepository;
 
     protected void configure(HttpSecurity http) throws Exception {
         http
