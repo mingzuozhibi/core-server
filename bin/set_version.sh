e4b9270b682ec05d2712a38cb3f95701577b31aa
fuhaiwei <fuhaiwei@126.com>
2020-04-24 22:42:59 +0800

    feat(set_version.sh): Set var before call

--- bin/set_version.sh
+++ bin/set_version.sh
@@ -3,16 +3,20 @@
 # 跳转到项目根目录
 cd `dirname $0`/..
 
-CallRelease() {
-    git flow release start v$1
+version="$1"
+version_text="v$1"
+option2="$2"
 
-    ./mvnw versions:set -DnewVersion=$1
+CallRelease() {
+    git flow release start $version_text
+
+    ./mvnw versions:set "-DnewVersion=$version"
     ./mvnw versions:commit
 
     git add pom.xml
-    git commit -m "chore: set version to v$1"
+    git commit -m "chore: set version to $version"
 
-    git flow release finish v$1
+    git flow release finish $version_text
 }
 
 CallPushAll() {
@@ -22,17 +26,17 @@ CallPushAll() {
   echo "开始推送分支 master"
   git push origin master
 
-  echo "开始推送标签 v$1"
-  git push origin v$1
+  echo "开始推送标签 $version_text"
+  git push origin $version_text
 }
 
-if [[ $2 == "-a" ]]; then
+if [[ $option2 == "-a" ]]; then
   CallRelease
   CallPushAll
   exit 0
 fi
 
-if [[ $2 == "-f" ]]; then
+if [[ $option2 == "-f" ]]; then
   CallPushAll
 else
   CallRelease
