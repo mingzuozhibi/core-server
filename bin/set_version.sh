ac20afb772209deb53bf5dca3f296be575c59f28
fuhaiwei <fuhaiwei@126.com>
2020-04-24 18:31:03 +0800

    feat(set_version.sh): Add push options

--- bin/set_version.sh
+++ bin/set_version.sh
@@ -1,18 +1,39 @@
 #!/usr/bin/env bash
 
-# 项目根目录
+# 跳转到项目根目录
 cd `dirname $0`/..
 
-# 准备发布新版本
-git flow release start v$1
+CallRelease() {
+    git flow release start v$1
 
-# 更新 pom.xml 版本号
-./mvnw versions:set -DnewVersion=$1
-./mvnw versions:commit
+    ./mvnw versions:set -DnewVersion=$1
+    ./mvnw versions:commit
 
-# 提交 pom.xml 版本号
-git add .
-git commit -m "chore: set version to v$1"
+    git add pom.xml
+    git commit -m "chore: set version to v$1"
 
-# 发布新版本
-git flow release finish v$1
+    git flow release finish v$1
+}
+
+CallPushAll() {
+  echo "开始推送分支 develop"
+  git push origin develop
+
+  echo "开始推送分支 master"
+  git push origin master
+
+  echo "开始推送标签 v$1"
+  git push origin v$1
+}
+
+if [ $2 -eq "-a" ]; then
+  CallRelease
+  CallPushAll
+  exit 0
+fi
+
+if [ $2 -eq "-f" ]; then
+  CallPushAll
+else
+  CallRelease
+fi
