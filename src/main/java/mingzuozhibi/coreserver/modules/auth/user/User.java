42651c0c990016d2105f39e779b9194d05976529
fuhaiwei <fuhaiwei@126.com>
2020-04-25 11:44:28 +0800

    feat(user): Set fetch = FetchType.EAGER

--- src/main/java/mingzuozhibi/coreserver/modules/auth/user/User.java
+++ src/main/java/mingzuozhibi/coreserver/modules/auth/user/User.java
@@ -46,7 +46,7 @@ public class User extends BaseModel {
 
     public static final Set<String> ALL_ROLES = Set.of("Guest", "Login", "DiscAdmin", "UserAdmin", "RootAdmin");
 
-    @ElementCollection
+    @ElementCollection(fetch = FetchType.EAGER)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
     private Set<String> roles = new HashSet<>();
 
