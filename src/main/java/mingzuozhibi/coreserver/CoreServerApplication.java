f4d19f47546628d1de9f3dfa9639b9dfaab134fb
fuhaiwei <fuhaiwei@126.com>
2020-05-04 10:36:41 +0800

    feat(spring): Add app start msgs

--- src/main/java/mingzuozhibi/coreserver/CoreServerApplication.java
+++ src/main/java/mingzuozhibi/coreserver/CoreServerApplication.java
@@ -1,8 +1,11 @@
 package mingzuozhibi.coreserver;
 
+import mingzuozhibi.coreserver.commons.message.enums.Index;
+import mingzuozhibi.coreserver.commons.message.support.MsgsHelper;
 import mingzuozhibi.coreserver.commons.support.ReflectUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,7 +15,9 @@ public class CoreServerApplication implements WebMvcConfigurer {
 
     public static void main(String[] args) {
         ReflectUtils.disableAccessWarnings();
-        SpringApplication.run(CoreServerApplication.class, args);
+        var context = SpringApplication.run(CoreServerApplication.class, args);
+        var msgs = context.getBean(MsgsHelper.class).with(Index.Default);
+        msgs.notify("core-server已经启动");
     }
 
 }
